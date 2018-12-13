3dc9b0403b89451ca6cc9bc2fa8c631300770b5e
Jimmy1244 <a0976625415@gmail.com>
2018-12-14 03:48:42 +0800

    connectionAndRegister
    我把註冊和連接資料庫先做好

---
+++ dbconnect.php
@@ -0,0 +1,13 @@
+<?php
+	$user = "admin";
+	$passwd = "ntoucse";
+	try{
+		$db = new PDO('mysql:host=localhost; dbname=00557043,34,19; charset=utf8', $user, $passwd);
+		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+	}
+	catch(PDOException $e){
+		Print "ERROR!:".$e->message();
+		die();
+	}
+?>
